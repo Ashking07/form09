d33def2586cfa2ed745d68e6dc324aa227631483
Ashking07 <ashwinkapile2002@gmail.com>
2023-03-20 22:10:31 +0530

    .

--- app.js
+++ app.js
@@ -7,39 +7,3 @@ const firebaseConfig = {
     appId: "1:720672914752:web:2869049f0f7aaba513cea5",
     measurementId: "G-J9WSHQB8P2"
   };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-// Reference messages collection
-var messagesRef = firebase.database().ref('messages');
-
-// Listen for form submit
-document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-// Submit form
-function submitForm(e){
-    e.preventDefault();
-    
-    // Get values
-    var name = getInputVal('name');
-    var company = getInputVal('company');
-    var email = getInputVal('email');
-    var phone = getInputVal('phone');
-    var message = getInputVal('message');
-    
-    // Save message
-    saveMessage(name, company, email, phone, message);
-    
-    // Show alert
-    document.querySelector('.alert').style.display = 'block';
-    
-    // Hide alert after 3 seconds
-    setTimeout(function(){
-        document.querySelector('.alert').style.display = 'none';
-    },3000);
-    
-    // Clear form
-    document.getElementById('contactForm').reset();
-    }
